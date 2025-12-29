08549337256f230c3569c757d66474d96175a398
berny.hunter <bernyhunter@hotmail.com>
2025-12-29 23:35:19 +0100

    supprime extra fichiers / edit IP

--- app.js
+++
@@ -1,8 +0,0 @@
-import { DataManager } from './dataManager.js';
-import { UIManager } from './uiManager.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const dm = new DataManager();
-    const ui = new UIManager(dm); 
-    ui.init();
-});
